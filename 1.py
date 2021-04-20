6cb088b22478456f1cd7752b32b8371c81b8623b
nder1996 <ander_2030@hotmail.com>
2021-04-20 14:07:52 -0500

    V-2.0

--- 1.py
+++ 1.py
@@ -5,40 +5,26 @@ import os
 
 
 
-def Validador(Num):
-  if len(Num)==1:
-   Busq = re.match(r'[\d]',Num)
-   if Busq!=None:
-    Jugar()
-   else: 
-     os.system ("cls")
-     print("---------------------------") 
-     print("Ingrese Solo Un Numero")
-     print("---------------------------\n") 
-     Jugar()
-  else:
-    os.system ("cls")
-    print("---------------------------")  
-    print("Ingrese Solo Un Numero")
-    print("---------------------------\n") 
-    Jugar()
 
 def TirarDados(): 
    a=0;b=6;d=0
+   print("\n-----------------------------------------------------------------")
    for j in range (2):
        c=random.randint(0,6)
-       print("Este es el numero que arrojo el dados  : ",c)
+       print("\nEste es el numero que arrojo el dados  : ",c)
        d+=c
-   print("Este es el acumulado de los numeros arrojado por los dado : " , d)
+   print("\nEste es el acumulado de los numeros arrojado por los dado : " , d)
+   print("\n-----------------------------------------------------------------\n")
 
 def Jugar():
-    i=0
-    while i>=0 or i==-1:
+    Num=""
+    while Num!="-1":
       TirarDados()   
-      print("\nÂ¿Desea tirar los dados?,ingrese cualquier numero para seguir jugando")
+      print("\n¿Desea tirar los dados?,ingrese cualquier numero para seguir jugando")
       Num=input("De lo contrario puede ingresar el numero -1 : ")
-      Validador(Num)
-
-
-
+      
+      
 Jugar()
+
+
+
